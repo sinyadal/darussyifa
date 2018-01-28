766ab83d08a114cd829c11c010a199cf0b527e11
sinyadal <23078942+sinyadal@users.noreply.github.com>
2018-01-28 10:00:11 +0800

    Setup uikit env

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,17 +1,15 @@
 
 window._ = require('lodash');
 
-/**
- * We'll load jQuery and the Bootstrap jQuery plugin which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
+import Icons from 'uikit/dist/js/uikit-icons';
 
 try {
     window.$ = window.jQuery = require('jquery');
 
-    require('bootstrap-sass');
+    window.UIkit = require('uikit');
+    UIkit.use(Icons);
 } catch (e) {}
+
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
