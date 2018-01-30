7a205616ca7e203b8e2fdb384347903eea46546f
sinyadal <v0xe7y2cl@protonmail.com>
2018-01-30 08:33:27 +0800

    Change redirect url

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::resource('/patient', 'PatientController');
 Route::post('/search', 'PatientController@search');
